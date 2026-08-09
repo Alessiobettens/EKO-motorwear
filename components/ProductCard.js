12ff9c129e2c5dcd94b4c00dfd5de104a27f71de
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-09 23:18:23 +0200

    Display products with images and prices

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,25 +1,45 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function ProductCard({ product }) {
   return (
     <View style={styles.card}>
-      <Text style={styles.name}>
-        {product.fieldData.name}
-      </Text>
+      <Image
+        source={{
+          uri: product.fieldData.afbeelding.url,
+        }}
+        style={styles.image}
+      />
+
+      <Text style={styles.name}>{product.fieldData.name}</Text>
+
+      <Text style={styles.price}>€ {product.fieldData.prijs}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     padding: 15,
-    marginVertical: 8,
+    marginVertical: 10,
     borderRadius: 10,
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
   },
 
   name: {
     fontSize: 18,
     fontWeight: "bold",
+    marginTop: 10,
+  },
+
+  price: {
+    color: "green",
+    fontSize: 16,
+    marginTop: 5,
   },
 });
--- screens/ProductenScreen.js
+++ screens/ProductenScreen.js
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-} from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
 import ProductCard from "../components/ProductCard";
-
 
 export default function ProductenScreen() {
   const [products, setProducts] = useState([]);
@@ -22,30 +16,32 @@ export default function ProductenScreen() {
         "https://api.webflow.com/v2/collections/6a5bbd773a372fa8a1b71610/items",
         {
           headers: {
-            Authorization: "Bearer 450ac81e27737ff9e1f75408ddd86f36ebdeb5c3bcbec7817c2ca2daad3f8b93",
+            Authorization:
+              "Bearer 450ac81e27737ff9e1f75408ddd86f36ebdeb5c3bcbec7817c2ca2daad3f8b93",
             accept: "application/json",
           },
-        }
+        },
       );
 
       const data = await response.json();
 
       console.log(data);
 
+      console.log(data.items[0].fieldData);
       setProducts(data.items);
     } catch (error) {
       console.log(error);
     }
   };
 
+  console.log(products[0]);
+
   return (
     <View style={styles.container}>
       <FlatList
         data={products}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <ProductCard product={item} />
-        )}
+        renderItem={({ item }) => <ProductCard product={item} />}
       />
     </View>
   );

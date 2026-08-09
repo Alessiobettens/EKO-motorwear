a55a63f82e50ff9d26d33889a3fd2fb412b7364d
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-09 17:37:23 +0200

    Added tab navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>EKO-MOTORWEAR</Text>
+
+      <Text style={styles.subtitle}>Premium motorkleding en accessoires</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#111",
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#ff6600",
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: "white",
+    marginTop: 10,
+  },
+});

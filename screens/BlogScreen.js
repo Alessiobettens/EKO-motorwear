ee3029c802299bc51f560bb3b95522a3d342a044
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-10 17:25:38 +0200

    Fetch blogs from Webflow API

--- screens/BlogScreen.js
+++ screens/BlogScreen.js
@@ -1,9 +1,41 @@
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, FlatList, Text } from "react-native";
 
-export default function NieuwsScreen() {
+export default function BlogScreen() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
+
+  const getBlogs = async () => {
+    try {
+      const response = await fetch(
+        "https://api.webflow.com/v2/collections/6a70a789e71cc80ac8866f7b/items",
+        {
+          headers: {
+            Authorization:
+              "Bearer 450ac81e27737ff9e1f75408ddd86f36ebdeb5c3bcbec7817c2ca2daad3f8b93",
+            accept: "application/json",
+          },
+        },
+      );
+
+      const data = await response.json();
+
+      setBlogs(data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View>
-      <Text>Nieuws Screen</Text>
+      <FlatList
+        data={blogs}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text>{item.fieldData.name}</Text>}
+      />
     </View>
   );
 }

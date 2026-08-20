362a1c0d820859614cc5778e3ff1d297242086ab
Alessiobettens <alessio.bettens@student.ehb.be>
2026-08-20 21:53:29 +0200

    add service screen + contact information

--- screens/ServiceScreen.js
+++ screens/ServiceScreen.js
@@ -1,9 +1,50 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { ScrollView, Text, View, StyleSheet } from "react-native";
 
-export default function AboutScreen() {
+export default function ServiceScreen() {
   return (
-    <View>
-      <Text>Service Screen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Service</Text>
+
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>📍 Contact</Text>
+
+        <Text>
+          EKO Motorwear{"\n"}
+          Pierstraat ...{"\n"}
+          Kontich{"\n\n"}
+          info@ekomotorwear.be{"\n"}
+          +32 ...
+        </Text>
+      </View>
+
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+
+  card: {
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 15,
+    borderRadius: 10,
+  },
+
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
+
